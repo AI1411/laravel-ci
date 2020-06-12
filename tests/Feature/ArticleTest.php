30f0d727e7b7aeb7afe637aeb7848970ed05d6ce
AI1411 <goethe0719@gmail.com>
2020-06-12 12:17:38 +0900

    いいねテスト追加

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Article;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -19,6 +20,35 @@ class ArticleTest extends TestCase
         $article = factory(Article::class)->create();
 
         $result = $article->isLikedBy(null);
+
+        $this->assertFalse($result);
+    }
+
+    /**
+     * @test
+     */
+    public function isLikedByTheUser()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $article->likes()->attach($user);
+
+        $result = $article->isLikedBy($user);
+
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @test
+     */
+    public function isLikedByAnother()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $another = factory(User::class)->create();
+        $article->likes()->attach($another);
+
+        $result = $article->isLikedBy($user);
 
         $this->assertFalse($result);
     }
